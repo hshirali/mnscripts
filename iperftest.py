964af06b7f8231edffd8e0b95da7d1b6bd664456
hshirali <hshirali@gmail.com>
2016-07-08 20:07:43 +0430

    dasd

---
+++ iperftest.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+from mininet.log import info, error, debug, output, warn
+from mininet.util import (quietRun, fixLimits, numCores, ensureRoot,
+                          macColonHex, ipStr, ipParse, netParse, ipAdd,
+                          waitListening)
+
+
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def build(self, n=2):
+        switch = self.addSwitch('s1')
+        for h in range(n):
+            # Each host gets 50%/n of system CPU
+            host = self.addHost('h%s' % (h + 1),
+               cpu=.5/n)
+            # 10 Mbps, 5ms delay, 10% loss, 1000 packet queue
+            self.addLink(host, switch,
+               bw=10, delay='5ms', use_htb=True)
+
+
+def myiperf(self, hosts=None, l4Type='TCP', udpBw='10M', fmt=None,
+          seconds=5, port=5001):
+    """Run iperf between two hosts.
+       hosts: list of hosts; if None, uses first and last hosts
+       l4Type: string, one of [ TCP, UDP ]
+       udpBw: bandwidth target for UDP test
+       fmt: iperf format argument if any
+       seconds: iperf time to transmit
+       port: iperf port
+       returns: two-element array of [ server, client ] speeds
+       note: send() is buffered, so client rate can be much higher than
+       the actual transmission rate; on an unloaded system, server
+       rate should be much closer to the actual receive rate"""
+
+
+    hosts = hosts or [self.hosts[0], self.hosts[-1]]
+    assert len(hosts) == 2
+    client, server = hosts
+    output('*** Iperf: testing', l4Type, 'bandwidth between',
+           client, 'and', server, '\n')
+    server.cmd('killall -9 iperf')
+    iperfArgs = 'iperf -p %d ' % port
+    bwArgs = ''
+    if l4Type == 'UDP':
+        iperfArgs += '-u '
+        bwArgs = '-b ' + udpBw + ' '
+    elif l4Type != 'TCP':
+        raise Exception('Unexpected l4 type: %s' % l4Type)
+    if fmt:
+        iperfArgs += '-f %s ' % fmt
+    server.sendCmd(iperfArgs + '-s')
+    if l4Type == 'TCP':
+        if not waitListening(client, server.IP(), port):
+            raise Exception('Could not connect to iperf on port %d'
+                            % port)
+    cliout = client.cmd(iperfArgs + '-t %d -c ' % seconds +
+                        server.IP() + ' ' + bwArgs)
+    debug('Client output: %s\n' % cliout)
+    server.sendInt()
+    servout = server.waitOutput()
+    debug('Server output: %s\n' % servout)
+    result = [self._parseIperf(servout), self._parseIperf(cliout)]
+    if l4Type == 'UDP':
+        result.insert(0, udpBw)
+    output('*** Results: %s\n' % result)
+    return result
+def perfTest():
+    "Create network and run simple performance test"
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo=topo,
+                  host=CPULimitedHost, link=TCLink)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    #net.pingAll()
+    print "Testing bandwidth between h1 and h4"
+    h1, h4 = net.get('h1', 'h4')
+    myiperf(net,(h1, h4))
+    
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    perfTest()
---
+++ topotree.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+
+from mininet.topo import Topo
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.log import setLogLevel
+#from mininet.topolib import TreeNet
+from myTree import TreeNet
+
+def run( controllers ):
+   # net = Mininet( topo=topo, controller=None, autoSetMacs=True )
+    net = TreeNet( depth=3, fanout=3, controller=None)
+    ctrl_count = 01
+    for controllerIP in controllers:
+        net.addController( 'c%d' % ctrl_count, RemoteController, ip=controllerIP )
+        ctrl_count += 1
+    net.start()
+    CLI( net )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    if len( sys.argv ) > 1:
+        controllers = sys.argv[ 1: ]
+    else:
+        print 'Usage: startnet <c0 IP> <c1 IP> ...'
+        exit( 1 )
+    run( controllers )
+
